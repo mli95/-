9939df18f021cafe5d3e0101991af9a83ee0bcae
mli95 <m.1995@foxmail.com>
2021-02-19 11:04:46 +0800

    Update qianshou.js

--- qianshou.js
+++ qianshou.js
@@ -1,12 +1,253 @@
 var obj = JSON.parse($response.body);
  obj={
-  "vip": [
-   {
-    "id": 9005757,
-    "auth_type": 1,
-    "auth_value": 1999999999
-   }
-  ]
+  "id": 13988416,
+  "info": {
+    "name": "os",
+    "realName": "王**",
+    "age": 25,
+    "gender": "male",
+    "description": "比较简单\n\n喜欢拍拍照 喜欢猫啊狗啊\n\n有趣的灵魂 需要慢慢了解\n\n就这样吧\n快找我聊天呀",
+    "height": 170,
+    "weight": 0,
+    "idCard": "332526**********10",
+    "zodiac": "capricorn",
+    "birthDate": "1995-07-28T00:00:00Z",
+    "wealth": {
+      "car": {
+        "ownType": 0
+      },
+      "house": {
+        "ownType": 0,
+        "houseAddress": ""
+      },
+      "income": {
+        "from": 600000,
+        "to": 999999
+      }
+    },
+    "job": {
+      "industry": "",
+      "position": "自由职业者"
+    },
+    "education": {
+      "school": "浙江工商大学杭州商学院",
+      "level": 4
+    },
+    "life": {
+      "residence": "510100000000",
+      "residenceChangeAvailable": false,
+      "homeTown": "110000000000",
+      "maritalStatus": "single"
+    },
+    "avatar": {
+      "order": 0,
+      "mediaType": "image/jpeg",
+      "parameters": [
+        {
+          "name": "format",
+          "values": [
+            "128x128",
+            "180x180",
+            "480x480",
+            "640x640",
+            "blur",
+            "blurRetina",
+            "max_180xX",
+            "max_480xX",
+            "max_640xX",
+            "max_720xX",
+            "max_960xX"
+          ]
+        }
+      ],
+      "video": null,
+      "audio": null,
+      "image": {
+        "url": "https://s1.qianshouapp.cn/v2/profile-images/eyJpZCI6IklKTFI2R0hXUkREV0RPRjNIUE9WWlhHRUhHU1RBVDA3IiwidyI6MTEyMiwiaCI6OTY5LCJkIjowLCJtdCI6ImltYWdlL2pwZWciLCJkaCI6Nzk4MDczMjEwNTQ1MDU2NTgzfQ",
+        "width": 1122,
+        "height": 969,
+        "dhash": 798073210545056600
+      },
+      "description": "",
+      "tag": 0
+    },
+    "backgroundImage": {
+      "url": "https://s1.qianshouapp.cn/v2/profile-images/eyJpZCI6IjdBTkZIQzJJTzQzNVNLWTVPSjM2UU1XU1lTTEVVNSIsInciOjEwMjksImgiOjQ5OCwiZCI6MCwibXQiOiJpbWFnZS9qcGVnIiwiZGgiOjUyODg3NDI2OTQzNDIxMjE4MTksImFiIjowfQ",
+      "index": 1
+    }
+  },
+  "tags": null,
+  "answers": null,
+  "medias": [
+    {
+      "order": 1,
+      "mediaType": "image/jpeg",
+      "parameters": [
+        {
+          "name": "format",
+          "values": [
+            "128x128",
+            "180x180",
+            "480x480",
+            "640x640",
+            "blur",
+            "blurRetina",
+            "max_180xX",
+            "max_480xX",
+            "max_640xX",
+            "max_720xX",
+            "max_960xX"
+          ]
+        }
+      ],
+      "video": null,
+      "audio": null,
+      "image": {
+        "url": "https://s1.qianshouapp.cn/v2/profile-images/eyJpZCI6IkVYSERDM0hJTUYzWTVTWkNDQU9VVERXT01GRUozSjA2IiwidyI6MTA4MCwiaCI6MTA4MCwiZCI6MCwibXQiOiJpbWFnZS9qcGVnIiwiZGgiOjE2NjczNTYxOTgyMjI4NzMwNzcxfQ",
+        "width": 1080,
+        "height": 1080,
+        "dhash": 16673561982228730000
+      },
+      "description": "等等",
+      "tag": 1
+    },
+    {
+      "order": 2,
+      "mediaType": "image/jpeg",
+      "parameters": [
+        {
+          "name": "format",
+          "values": [
+            "128x128",
+            "180x180",
+            "480x480",
+            "640x640",
+            "blur",
+            "blurRetina",
+            "max_180xX",
+            "max_480xX",
+            "max_640xX",
+            "max_720xX",
+            "max_960xX"
+          ]
+        }
+      ],
+      "video": null,
+      "audio": null,
+      "image": {
+        "url": "https://s1.qianshouapp.cn/v2/profile-images/eyJpZCI6IjdNT0RKRkFDVURLVVY3S1k3TzNYNEFDM0RXUEJVNzA2IiwidyI6MTA4MCwiaCI6MTA4MCwiZCI6MCwibXQiOiJpbWFnZS9qcGVnIiwiZGgiOjE0MTUzNDk0MTc0OTMzMTU5MDI0fQ",
+        "width": 1080,
+        "height": 1080,
+        "dhash": 14153494174933160000
+      },
+      "description": "cat",
+      "tag": 2
+    },
+    {
+      "order": 3,
+      "mediaType": "image/jpeg",
+      "parameters": [
+        {
+          "name": "format",
+          "values": [
+            "128x128",
+            "180x180",
+            "480x480",
+            "640x640",
+            "blur",
+            "blurRetina",
+            "max_180xX",
+            "max_480xX",
+            "max_640xX",
+            "max_720xX",
+            "max_960xX"
+          ]
+        }
+      ],
+      "video": null,
+      "audio": null,
+      "image": {
+        "url": "https://s1.qianshouapp.cn/v2/profile-images/eyJpZCI6IllPUUZaM05KVzZHS0dZQTVCTEJaQ01EVDVXVFQyVjA2IiwidyI6MTA4MCwiaCI6MTkyMCwiZCI6MCwibXQiOiJpbWFnZS9qcGVnIiwiZGgiOjQzODg3MDUyMzg3NzQzOTAzMn0",
+        "width": 1080,
+        "height": 1920,
+        "dhash": 438870523877439040
+      },
+      "description": "点滴",
+      "tag": 3
+    },
+    {
+      "order": 4,
+      "mediaType": "image/jpeg",
+      "parameters": [
+        {
+          "name": "format",
+          "values": [
+            "128x128",
+            "180x180",
+            "480x480",
+            "640x640",
+            "blur",
+            "blurRetina",
+            "max_180xX",
+            "max_480xX",
+            "max_640xX",
+            "max_720xX",
+            "max_960xX"
+          ]
+        }
+      ],
+      "video": null,
+      "audio": null,
+      "image": {
+        "url": "https://s1.qianshouapp.cn/v2/profile-images/eyJpZCI6IjVHV0EyUjcyNVhWR1UyM1RCQUxUN1Q0SEdVN0dPVDA2IiwidyI6MTU0OCwiaCI6MTA3OCwiZCI6MCwibXQiOiJpbWFnZS9qcGVnIiwiZGgiOjEwNTk2NjA0NzEzMDU4NjkwMTR9",
+        "width": 1548,
+        "height": 1078,
+        "dhash": 1059660471305869000
+      },
+      "description": "dog",
+      "tag": 4
+    }
+  ],
+  "verifications": {
+    "identityVerified": true,
+    "pictureVerified": false
+  },
+  "membership": null,
+  "memberships": {
+    "see": {
+      "name": "see",
+      "active": true,
+      "expire": "2021-03-17T15:59:00.000+00:00",
+      "status": 0
+    },
+    "svip": {
+      "name": "svip",
+      "active": false,
+      "expire": "2020-10-15T15:59:00.000+00:00",
+      "status": 0
+    },
+    "vvip": {
+      "name": "vvip",
+      "active": true,
+      "expire": "2021-03-12T15:59:00.000+00:00",
+      "status": 0
+    }
+  },
+  "relationships": [
+    {
+      "relationType": 2,
+      "createdTime": "2021-02-14T03:35:48.365+00:00",
+      "updatedTime": 1613273748365451
+    }
+  ],
+  "status": "good",
+  "account": {
+    "mobile": "15826039251",
+    "status": 0
+  },
+  "createdTime": "2021-02-13T18:24:21.735+00:00",
+  "activeTime": "2021-02-19T03:02:57.468+00:00",
+  "matchingStatus": 0
 };
 $done({body: JSON.stringify(obj)});
 //
