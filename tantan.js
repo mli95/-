dc8277f3b058a27142eb0f8bf50952764c8d536b
mli95 <m.1995@foxmail.com>
2021-02-21 05:50:27 +0800

    Add files via upload

--- tantan.js
+++ tantan.js
@@ -1,71 +1,61 @@
-var obj = JSON.parse($response.body);
- obj={
-  "names": ["loveAD2:exp_newbutton_male", "loveAD4:maleA_one", "REV_diamondvip3:test", "profile_ver2_area:__exp1", "friendonlineV4_:__control"],
-	"groups": {
-		"REV_diamondvip3": "REV_diamondvip3:test",
-		"friendonlineV4_": "friendonlineV4_:__control",
-		"loveAD2": "loveAD2:exp_newbutton_male",
-		"loveAD4": "loveAD4:maleA_one",
-		"profile_ver2_area": "profile_ver2_area:__exp1"
-	},
-	"abHeader": "[{\"name\":\"loveAD2-exp_newbutton_male\",\"redirect\":false},{\"name\":\"loveAD4-maleA_one\",\"redirect\":false},{\"name\":\"REV_diamondvip3-test\",\"redirect\":false},{\"name\":\"profile_ver2_area-__exp1\",\"redirect\":true,\"rules\":[{\"path\":\"^/v2/users/%w+%??[^/]*$\"}]},{\"name\":\"friendonlineV4_-__control\",\"redirect\":false}]",
-	"properties": {
-		"REV_diamondvip3:test": {
-			"restart": false
-		},
-		"friendonlineV4_:__control": {
-			"restart": false
-		},
-		"loveAD2:exp_newbutton_male": {
-			"apk_md5": "8e067aadf94f545f81d68c482254ddaf",
-			"button": "限时参与内测",
-			"button_click": 1,
-			"button_link": "http://apk-ssl.tancdn.com/qianshou/qianshou-1.19.1_40.apk",
-			"button_link_iOS": "itms-apps://apps.apple.com/cn/app/id1475216610",
-			"ios_download_way": 0,
-			"list_button_text": "去看看",
-			"list_logo_pic": "http://s1.qianshouapp.cn/v1/ttx/profile/images/eyJpZCI6IlQyWFdPTFZHQURPNFU1VlY2U0lIMk00RkNBSUJKQyIsInciOjYxMiwiaCI6MTUzLCJkIjowLCJtdCI6ImltYWdlL2pwZWciLCJkaCI6MzMyNTE3ODI4ODEwMjI1NjEzNiwiYWIiOjB9.png?format=max_1080xX",
-			"list_photo_pic": "http://s1.qianshouapp.cn/v1/ttx/profile/images/eyJpZCI6IllGRlhQREo1TUhNU0VSQ0pJS09YRDI2VEwyVjVWUSIsInciOjE4MCwiaCI6NjYsImQiOjAsIm10IjoiaW1hZ2UvanBlZyIsImRoIjozMjIxOTU1OTIyODQ4MDc2OTgwLCJhYiI6MH0.png?format=max_1080xX",
-			"list_slogan": "更多想恋爱的人在「牵手」等你",
-			"list_title_text": "诚意恋爱专区",
-			"main_title": "牵手APP",
-			"pic_photo": "http://s1.qianshouapp.cn/v1/ttx/profile/images/eyJpZCI6IjdTQzNHR01TUDVMWUhaT0xENlpOQjRVTEVGV0lHTSIsInciOjY4NiwiaCI6NTg4LCJkIjowLCJtdCI6ImltYWdlL2pwZWciLCJkaCI6NTMxMzE0ODU2NDI3NzExNzc2OSwiYWIiOjB9.png?format=max_1080xX##http://s1.qianshouapp.cn/v1/ttx/profile/images/eyJpZCI6IjU3VlRSMkZXQjVNSFJRUlNYTk9LNEdONTNEWE5URCIsInciOjY4NiwiaCI6NTg2LCJkIjowLCJtdCI6ImltYWdlL2pwZWciLCJkaCI6NTMxMzE4MTIxMDIwOTA5NDQ1MiwiYWIiOjB9.png?format=max_1080xX##http://s1.qianshouapp.cn/v1/ttx/profile/images/eyJpZCI6IlFVWFlXTVVRTlVZNVdJWVU3U1lLQVFWMzZFTFZCSSIsInciOjY4NiwiaCI6NTg4LCJkIjowLCJtdCI6ImltYWdlL2pwZWciLCJkaCI6Mzg4Nzc4OTk3MzA5NzUxMDI1NywiYWIiOjB9.png?format=max_1080xX##http://s1.qianshouapp.cn/v1/ttx/profile/images/eyJpZCI6IkNFSUdUMkNSTDY0VFhHMjM3TEpSTVdNV04yWVVZViIsInciOjY4NiwiaCI6NTg4LCJkIjowLCJtdCI6ImltYWdlL2pwZWciLCJkaCI6NTYwMTM4MTIxODg1MzE0MzA2NSwiYWIiOjB9.png?format=max_1080xX##http://s1.qianshouapp.cn/v1/ttx/profile/images/eyJpZCI6IkdXTDRBTEk1QTVZTjJGRkxFQkNEWFlGQ1pZWVhVNSIsInciOjY4NiwiaCI6NTg4LCJkIjowLCJtdCI6ImltYWdlL2pwZWciLCJkaCI6NzA1NjAzNzQ3MDU1OTE1ODc5MywiYWIiOjB9.png?format=max_1080xX##http://s1.qianshouapp.cn/v1/ttx/profile/images/eyJpZCI6IlUzVTZZNFVVQVlOVzdRM1ZEUEQzTURMNEtUMkhHNCIsInciOjY4NiwiaCI6NTg4LCJkIjowLCJtdCI6ImltYWdlL2pwZWciLCJkaCI6NjQ1NzA5Mzc4NTY5MzU4ODA1NCwiYWIiOjB9.png?format=max_1080xX",
-			"redpoint_rule": "0##1##1",
-			"restart": false,
-			"slogan": "多重认证##丰富资料##高效社交",
-			"titlebar_text": "探探出品-诚意恋爱APP",
-			"vice_title": "探探邀你内测",
-			"web_link": "http://marketh5.tantanapp.com/qianshou_female_03/?speed=true"
-		},
-		"loveAD4:maleA_one": {
-			"apk_md5": "a55d60f43d16a1ecd0d7938fef64e551",
-			"button_link_iOS": "itms-apps://apps.apple.com/cn/app/id1475216610",
-			"download_link": "http://apk-ssl.tancdn.com/qianshou/qianshou-1.35.1_66.apk",
-			"list_button_text": "去看看",
-			"list_logo_pic": "http://s1.qianshouapp.cn/v1/ttx/profile/images/eyJpZCI6IlQyWFdPTFZHQURPNFU1VlY2U0lIMk00RkNBSUJKQyIsInciOjYxMiwiaCI6MTUzLCJkIjowLCJtdCI6ImltYWdlL2pwZWciLCJkaCI6MzMyNTE3ODI4ODEwMjI1NjEzNiwiYWIiOjB9.png?format=max_1080xX",
-			"list_photo_pic": "http://s1.qianshouapp.cn/v1/ttx/profile/images/eyJpZCI6IllGRlhQREo1TUhNU0VSQ0pJS09YRDI2VEwyVjVWUSIsInciOjE4MCwiaCI6NjYsImQiOjAsIm10IjoiaW1hZ2UvanBlZyIsImRoIjozMjIxOTU1OTIyODQ4MDc2OTgwLCJhYiI6MH0.png?format=max_1080xX",
-			"list_show_rule": "0",
-			"list_show_times": 8,
-			"list_slogan": "更多想恋爱的人在「牵手」等你",
-			"list_title_text": "诚意恋爱专区",
-			"new_list_avatar": "http://s1.qianshouapp.cn/v1/ttx/group/images/eyJpZCI6Ikw2RzZNQk1TRUlKNEY2UUtNS0k3NkozVFBVR0tJWSIsInciOjIxNiwiaCI6MjE2LCJkIjowLCJtdCI6ImltYWdlL2pwZWciLCJkaCI6MTY3NTYyNjE0NjAyODE4OTgyMjQsImFiIjowfQ",
-			"new_list_text": "牵手APP",
-			"new_list_vice_text": "邀请你加入高品质婚恋平台",
-			"pic_popup1": "http://s1.qianshouapp.cn/v1/ttx/profile/images/eyJpZCI6IlNUTU5aVTNKSU4zQUdDRzdGU0pXQU1LTFkyVEdaNiIsInciOjc2OCwiaCI6MTE5NCwiZCI6MCwibXQiOiJpbWFnZS9qcGVnIiwiZGgiOjMxMjcwMTU2ODcxNDQ4MDU5NDMsImFiIjowfQ.png?format=max_1080xX",
-			"pic_popup2": "http://s1.qianshouapp.cn/v1/ttx/profile/images/eyJpZCI6IklHQ1BKVzVSNkc0QlBJTEFVNjZCUFlNR01TRFhIUiIsInciOjc2OCwiaCI6MTE4OCwiZCI6MCwibXQiOiJpbWFnZS9qcGVnIiwiZGgiOjc0NTIzMjUyNzQzODIyNTIxNDEsImFiIjowfQ.png?format=max_1080xX",
-			"pic_popup3": "http://s1.qianshouapp.cn/v1/ttx/profile/images/eyJpZCI6IjU0WFhVWVkyM1lCNDVVWUFKVDdKN0k3V0MzSFA2TyIsInciOjc2OCwiaCI6MTE4OCwiZCI6MCwibXQiOiJpbWFnZS9qcGVnIiwiZGgiOjgyMDg2MTY3MTc0MTU1MDcyNzMsImFiIjowfQ.png?format=max_1080xX",
-			"redpoint_rule": "0##1##1",
-			"register_days_limit": 3,
-			"restart": false,
-			"show_rule": "0##2##4",
-			"titlebar_text": "探探出品-诚意恋爱APP",
-			"web_link": "http://m.qianshouapp.cn/m/land/#/match-v2?gender=female"
-		},
-		"profile_ver2_area:__exp1": {
-			"restart": false
-		}
-	},
-	"expiredSince": "2021-02-28T03:54:47.112371+0000"
+
+/**
+ * @fileoverview Template to compose HTTP reqeuest.
+ * 
+ */
+
+const url = `https://web-api.tantanapp.com/v2/users/me/relationships/40842526`;
+const method = `PUT`;
+const headers = {
+'Sec-Fetch-Dest' : `empty`,
+'Connection' : `keep-alive`,
+'Accept-Encoding' : `gzip, deflate, br`,
+'Geolocation' : `geo:39.9215,116.4431;u=80`,
+'Content-Type' : `application/json`,
+'Sec-Fetch-Site' : `same-site`,
+'Origin' : `https://web.tantanapp.com`,
+'Cache-Control' : `no-cache`,
+'User-Agent' : `Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.74`,
+'Sec-Fetch-Mode' : `cors`,
+'Cookie' : `uc_100010=1%2C100010%2ClsQdUWDkr2ayPW%2BO23jBoeIhqH5K52iRjuXpfZHbk6Q%3D; tt_client_id=100010`,
+'Host' : `web-api.tantanapp.com`,
+'Referer' : `https://web.tantanapp.com/`,
+'Accept-Language' : `zh-CN`,
+'Accept' : `*/*`
 };
-$done({body: JSON.stringify(obj)});
-//
+const body = `{
+	"state": "disliked",
+	"createdTime": "2021-02-20T20:54:15.402000+0000",
+	"trackDictionary": {
+		"pic_uploads": "5",
+		"normal_pic": "1",
+		"actiontype": "click",
+		"update_relationships_source": "suggest_users_home",
+		"moments_user_id": "408425269",
+		"actionname": "dislike",
+		"update_relationships_page_id": "p_suggest_users_home_view",
+		"pic_num": "1",
+		"suggest_info": "{\"ActorInfo\":{\"UserId\":\"429225332\",\"CreateTime\":\"2021-02-05 00:46:05\",\"Poplularity\":0.685,\"LooksLevel\":\"\",\"SettingSearchMinAge\":20,\"SettingSearchMaxAge\":63,\"AdjustSearchMinAge\":0,\"AdjustSearchMaxeAge\":0,\"PMatched24h\":0,\"PLiked24h\":0,\"PSwiped24h\":3,\"PLikedBy24h\":76,\"PSwipedBy24h\":119,\"LikedRate\":0,\"UnprocessedReceiveLike\":-1},\"ReceiverInfo\":{\"UserId\":\"408425269\",\"CreateTime\":\"2020-10-30 19:29:27\",\"Poplularity\":0.275,\"LooksLevel\":\"normal\",\"SettingSearchMinAge\":16,\"SettingSearchMaxAge\":35,\"AdjustSearchMinAge\":0,\"AdjustSearchMaxeAge\":0,\"PMatched24h\":4,\"PLiked24h\":0,\"PSwiped24h\":0,\"PLikedBy24h\":4,\"PSwipedBy24h\":7,\"LikedRate\":0.9666079467606186,\"UnprocessedReceiveLike\":-1},\"SuggestDetail\":{\"RecallStrategy\":\"boost_default\",\"RankStrategy\":\"\",\"RerankStrategy\":\"\",\"RerankStrategyPositionBefore\":10,\"RerankStrategyPositionAfter\":10}}",
+		"pic_url": "https:\/\/auto.tancdn.com\/v1\/images\/eyJpZCI6IjNHNkRXMlY3QkJFU1ZDQzI3SzVUSkZZRUoyMjJGQzA2IiwidyI6MTQxOSwiaCI6MTc3MywiZCI6MCwibXQiOiJpbWFnZS9qcGVnIiwiZGgiOjczNjI5NDgwODM0MjQ3Mzk1MjB9",
+		"update_relationships_module_id": "e_dislikeButton",
+		"next_normal_pic": "1",
+		"create_time": "1613854455",
+		"IsWhiteBlock": "0",
+		"next_IsWhiteBlock": "0"
+	}
+}`;
+
+const myRequest = {
+    url: url,
+    method: method,
+    headers: headers,
+    body: body
+};
+
+$task.fetch(myRequest).then(response => {
+    console.log(response.statusCode + "\n\n" + response.body);
+    $done();
+}, reason => {
+    console.log(reason.error);
+    $done();
+});
